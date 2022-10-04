00540f1ecd8382eb0850a960b81c80c9dd483ed1
Cesar11780 <cramirezcu7@gmail.com>
2022-10-03 23:19:17 -0500

    Statefull interactivo con un boton

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:statefull_app/src/button.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: MyButton(),
+      debugShowCheckedModeBanner: false
+      )
+  );
+}
